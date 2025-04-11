cfa02f0e58b1619652b5198523e50b591e8c204e
gzamoraluis <gzamoraluis@gmail.com>
2025-04-11 06:02:02 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = 'wine-calculators-v1';
-const urlsToCache = [
+const ASSETS = [
+
     '/',
     '/index.html',
     '/so2-calculator.html',
@@ -10,20 +11,15 @@ const urlsToCache = [
     '/icons/icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
 });
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
